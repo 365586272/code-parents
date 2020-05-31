30a640c9d6eab51aaed45f48e027cb9429c28c29
徐鹏 <365586272@qq.com>
2020-05-31 17:09:51 +0800

    项目经理提交到本地库123

--- src/main/java/com/code/util/TestGit.java
+++ src/main/java/com/code/util/TestGit.java
@@ -3,7 +3,7 @@ package com.code.util;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("clone项目......,开发人员");
-        System.out.println("clone项目......,项目经理");
+        System.out.println("clone项目......,项目经理111");
         System.out.println("https://github.com/365586272/code-parents.git");
     }
 }
