7af0b2bc67da66328ce1ee529381cf66d27442e5
徐鹏 <365586272@qq.com>
2020-05-31 19:56:41 +0800

    解决冲突

--- src/main/java/com/code/util/TestGit.java
+++ src/main/java/com/code/util/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
         System.out.println("https://github.com/365586272/code-parents.git");
         System.out.println("dev1建立");
         System.out.println("再次在dev1修改");
+        System.out.println("第二次修改dev1");
     }
 }
