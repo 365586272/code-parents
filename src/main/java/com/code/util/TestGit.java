af5d0809f4283730c1c9eb6fde36a4bd598079c0
徐鹏 <365586272@qq.com>
2020-05-31 16:39:55 +0800

    开发人员提交

--- src/main/java/com/code/util/TestGit.java
+++ src/main/java/com/code/util/TestGit.java
@@ -3,5 +3,6 @@ package com.code.util;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("clone项目......");
+        System.out.println("https://github.com/365586272/code-parents.git");
     }
 }
