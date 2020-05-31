4bfb60ebad214e161c3002d9dcd76ab9512ad6df
徐鹏 <365586272@qq.com>
2020-05-31 15:20:52 +0800

    第一次提交到本地库

---
+++ src/main/java/com/code/constant/Constant.java
@@ -0,0 +1,32 @@
+package com.code.constant;
+
+/**
+ * Created by dm.
+ */
+public class Constant {
+
+    public static String template_path = "E:\\dmwork\\code-parent\\src\\main\\resources\\dm";
+
+    //实体model类生成地址
+    public static String model_save_path = "E:\\dmwork\\common\\dm-common\\dm-common-module\\src\\main\\java\\cn\\dm\\pojo";
+
+    public static String mapper_save_path = "E:\\dmwork\\common\\dm-common\\dm-common-dao\\src\\main\\resources\\mapper";
+    public static String class_mapper_save_path = "E:\\dmwork\\common\\dm-common\\dm-common-dao\\src\\main\\java\\cn\\dm\\mapper";
+
+    public static String client_dm_save_path = "E:\\dmwork\\common\\dm-common\\dm-common-client\\src\\main\\java\\cn\\dm\\client";
+    public static String client_dm_fallback_save_path = "E:\\dmwork\\common\\dm-common\\dm-common-client\\src\\main\\java\\cn\\dm\\fallback";
+    //item provider路径
+    public static String service_dm_item_save_path = "E:\\dmwork\\item-provider\\dm-item-provider\\src\\main\\java\\cn\\dm\\service";
+    //order provider路径
+    public static String service_dm_order_save_path = "E:\\dmwork\\order-provider\\dm-order-provider\\src\\main\\java\\cn\\dm\\service";
+    //pay provider路径
+    public static String service_dm_pay_save_path = "E:\\dmwork\\pay-provider\\dm-pay-provider\\src\\main\\java\\cn\\dm\\service";
+    //scheduler provider路径
+    public static String service_dm_scheduler_save_path = "E:\\dmwork\\scheduler-provider\\dm-scheduler-provider\\src\\main\\java\\cn\\dm\\service";
+    //user provider路径
+    public static String service_dm_user_save_path = "E:\\dmwork\\user-provider\\dm-user-provider\\src\\main\\java\\cn\\dm\\service";
+    //common provider路径
+    public static String service_dm_common_save_path = "E:\\dmwork\\base-provider\\dm-base-provider\\src\\main\\java\\cn\\dm\\service";
+
+
+}
